507ed43c3c5bc265be3bbee4270b52634f18280f
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-11-04 11:11:16 +0200

    Remove unused associations form the Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,6 @@
 class Book < ApplicationRecord
   has_many :comments, dependent: :destroy
 
-  has_many :book_categories, dependent: :destroy, foreign_key: 'book_id'
-
-  has_many :categories, through: :book_categories
-
   validates :title, presence: true, length: { minimum: 2, maximum: 40 }
 
   validates :author, presence: true, length: { minimum: 2, maximum: 40 }
